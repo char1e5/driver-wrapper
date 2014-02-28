3f0e98e1c786bc026c3da18eda5c2f711e142a3c
Charles Chu <cchu@opentable.com>
2014-02-27 18:15:07 -0800

    Add take screenshots function

--- spec/mocha-test.js
+++ spec/mocha-test.js
@@ -1,6 +1,9 @@
 var expect = require('chai').expect,
-test = require('selenium-webdriver/testing'),
-driver = require('../lib/driverwrapper');
+    driver = require('../lib/driverwrapper'),
+    test = require('../lib/runner'),
+    fs = require('fs');
+
+
 
 
 test.describe('OpenTable Location Search', function() {
@@ -16,6 +19,8 @@ test.describe('OpenTable Location Search', function() {
 
   test.it('should get restaurant search results', function() {
     browser.get('http://m.opentable.com');
+    browser.resize(1024,768);
+    browser.saveScreenshot('out0.png', fs);
     browser.getTitle().then(function(title) {
       expect(title).to.equal('Restaurants and Restaurant Reservations | OpenTable');
     });
@@ -24,17 +29,17 @@ test.describe('OpenTable Location Search', function() {
 
   test.it('should append query to title', function() {
     browser.get('http://m.opentable.com/#/location');
-
-    
-    // browser.findElement(driver.By.id('input-location')).click();
-    // browser.findElement(driver.By.id('input-location')).sendKeys('San Francisco, CA, United States');
-    // browser.findElement(driver.By.id('button-search')).click();
-
+    browser.resize(320,600);
 
     var locationInput = browser.ById('input-location');
-    var searchButton = browser.ById('button-search');
+    var searchButton = browser.ById('button-search'); 
+    browser.saveScreenshot('out1.png', fs);
+
     locationInput.click();
     locationInput.sendKeys('San Francisco, CA, United States');
+
+    browser.resize(768,1024);
+    browser.saveScreenshot('out2.png', fs);
     searchButton.click();
 
     browser.wait(function() {
