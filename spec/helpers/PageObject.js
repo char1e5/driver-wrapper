7a6bd9df438cbb9f0b7f883be942c333ad35c85e
Charles Chu <cchu@opentable.com>
2014-03-04 14:50:31 -0800

    Create a page object demo

---
+++ spec/helpers/PageObject.js
@@ -0,0 +1,30 @@
+var driver = require('../../lib/driverwrapper'),
+        fs = require('fs'),
+    expect = require('chai').expect;
+var browser;
+
+var PageObject = function(context) {
+  browser = driver.getBrowser('chrome');
+  this.browser = browser;
+}
+
+PageObject.prototype.visit = function(url) {
+  browser.get(url);
+};
+
+PageObject.prototype.saveScreenshot = function(path) {
+  browser.saveScreenshot(path, fs)
+};
+
+PageObject.element = function(method, css) {
+  this.prototype[method] = function() {
+    return browser.$(css);
+  };
+};
+
+
+PageObject.prototype.close = function() {
+  driver.quit(browser);
+}
+
+module.exports = PageObject;
